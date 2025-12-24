898634acdc1d6735234ea027d6c3c088c6e2e7d1
Thunderbeast5 <163726116+Thunderbeast5@users.noreply.github.com>
2025-12-24 21:44:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen grid place-items-center bg-red-100 text-red-500">
+      <h1 className="text-5xl font-bold">Hello</h1>
+    </main>
+  )
+}
+
+export default App
