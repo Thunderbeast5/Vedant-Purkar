d35be1b2db272ab732b607615ec354807bbe8171
Thunderbeast5 <163726116+Thunderbeast5@users.noreply.github.com>
2025-12-24 22:15:34 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import Spline from '@splinetool/react-spline';
+import Hero from './components/Hero';
 
-export default function App() {
+function App() {
   return (
-    /* h-screen ensures the container is exactly the height of the window */
-    /* w-full ensures it takes the full width */
-    /* overflow-hidden prevents any scrollbars from the 3D scene */
-    <main className="h-screen w-full overflow-hidden bg-[#E3E3E3]">
-      <Spline 
-        scene="https://prod.spline.design/aUdRZ09ud-nS-2ym/scene.splinecode" 
-        /* The className here ensures the internal canvas fills its parent */
-        className="w-full h-full"
-      />
-    </main>
-  );
+    <Hero />
+  )
 }
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Hero() {
+  return (
+    /* h-screen ensures the container is exactly the height of the window */
+    /* w-full ensures it takes the full width */
+    /* overflow-hidden prevents any scrollbars from the 3D scene */
+    <main className="h-screen w-full overflow-hidden bg-[#E3E3E3]">
+      <Spline 
+        scene="https://prod.spline.design/aUdRZ09ud-nS-2ym/scene.splinecode" 
+        /* The className here ensures the internal canvas fills its parent */
+        className="w-full h-full"
+      />
+    </main>
+  );
+}
